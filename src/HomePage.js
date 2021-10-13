356cab5becaaedd825c3ae2e2337c54af4c5d7bb
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-12 18:11:15 -0700

    Add homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-        HomePage
+        <Link to='/login'>
+          <div >
+            Log In
+          </div>
+        </Link>
+        <Link to='/signup'>
+          <div>
+            Sign Up
+          </div>
+        </Link>
       </div>
     )
   }
